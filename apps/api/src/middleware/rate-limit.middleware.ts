d364d9f9be2d684a6106f29cf36cff7e64a6cd62
Aditya Divakar <217085854+Ad1tya244@users.noreply.github.com>
2026-01-22 13:21:55 +0530

    Implemented API rate limiting(10 attempts per minute)

--- apps/api/src/middleware/rate-limit.middleware.ts
+++ apps/api/src/middleware/rate-limit.middleware.ts
@@ -1,9 +1,9 @@
 import rateLimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 login requests per window
-    message: "Too many login attempts from this IP, please try again after 15 minutes",
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 10, // Limit each IP to 5 login requests per window
+    message: { error: "Too many attempts from this device or network. Please try again in 1 minute" },
     standardHeaders: true,
     legacyHeaders: false,
 });
