7636d09d9fcc2599afdd26b50208fe86711f710f
Aditya Divakar <217085854+Ad1tya244@users.noreply.github.com>
2026-06-27 00:59:15 +0530

    fix: mark argon2 as external server components package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     poweredByHeader: false,
+    experimental: {
+        serverComponentsExternalPackages: ["argon2"],
+    },
     async headers() {
         const isProd = process.env.NODE_ENV === "production";
         const cspValue = isProd
